9fe7e9184280007b774a2f10608726f9c9c14fea
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-12-18 22:52:41 +0530

    swagger postmapping commit

--- src/main/java/com/techieey/codefun/controller/HotelController.java
+++ src/main/java/com/techieey/codefun/controller/HotelController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techieey.codefun.model.Hotel;
 import com.techieey.codefun.repository.HotelRepository;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/hotels")
@@ -21,4 +25,12 @@ public class HotelController{
         public List<Hotel> getAll(){
             return hotelRepository.findAll();
         }
+
+        @PostMapping("/post")
+        public Hotel persist(@RequestBody Hotel hotel){
+            hotelRepository.save(hotel);
+            return hotelRepository.findById(hotel.getId()).get();          
+        }
+
+
 }
--- src/main/java/com/techieey/codefun/model/Hotel.java
+++ src/main/java/com/techieey/codefun/model/Hotel.java
@@ -1,13 +1,20 @@
 package com.techieey.codefun.model;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Hotel {
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+    @NotNull
     private String country;
+    @NotNull
     private String city;
+    @NotNull
     private long phnumber;
     public Hotel(Integer id, String country, String city, long phnumber) {
         this.id = id;
